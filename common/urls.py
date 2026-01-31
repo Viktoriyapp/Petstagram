1b57a9db99dbf370827c94e1ec0bf842291b05d0
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-01-31 12:11:44 +0200

    Added more url patterns and views for every app

--- common/urls.py
+++ common/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from common import views
+
+app_name = 'common'
+
+urlpatterns = [
+    path('', views.home_page, name='home'),
 ]
--- pets/urls.py
+++ pets/urls.py
@@ -1,3 +1,14 @@
-urlpatterns = [
+from django.urls import path, include
 
+from pets import views
+
+app_name = 'pets'
+
+urlpatterns = [
+    path('add/', views.pet_add, name='add'),
+    path('<str:username>/pet/<slug:pet_slug>/', include([
+        path('', views.pet_details, name='details'),
+        path('edit/', views.pet_edit, name='edit'),
+        path('delete/', views.pet_delete, name='delete'),
+    ]))
 ]
