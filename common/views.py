3c559c5e1d8113f10646693c84758868695f91d5
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-02-10 19:47:55 +0200

    Added functionality for likes, for shares only locally and for clicking on tagged pets

--- common/views.py
+++ common/views.py
@@ -1,7 +1,35 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, resolve_url
+from pyperclip import copy
+
+from common.models import Like
+from photos.models import Photo
+
 
 # Create your views here.
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'common/home-page.html')
+    all_photos = (Photo.objects
+        .prefetch_related('tagged_pets', 'like_set')
+        .all())
+
+    context = {'all_photos': all_photos}
+
+    return render(request, 'common/home-page.html', context)
+
+
+def like_functionality(request: HttpRequest, photo_pk: int) -> HttpResponse:
+    like_object = Like.objects.filter(to_photo_id=photo_pk).first()
+
+    if like_object:
+        like_object.delete()
+    else:
+        Like.objects.create(to_photo_id=photo_pk,)
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_pk}')
+
+
+def share_functionality(request: HttpRequest, photo_pk: int) -> HttpResponse:
+    #This will work only on localhost as it copies the value on the server
+    copy(request.META.get('HTTP_REFERER')[:-1] + resolve_url('photos:details', photo_pk))
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_pk}')
--- pets/views.py
+++ pets/views.py
@@ -1,16 +1,29 @@
+from django.db.models import Prefetch
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from pets.models import Pet
+from photos.models import Photo
+
 
 # Create your views here.
 
 def pet_add(request: HttpRequest) -> HttpResponse:
     return render(request, 'pets/pet-add-page.html')
 
-def pet_details(request: HttpRequest, pk: int) -> HttpResponse:
-    return render(request, 'pets/pet-details-page.html')
+def pet_details(request: HttpRequest, username: str, pet_slug: str) -> HttpResponse:
+    pet = Pet.objects.prefetch_related(
+        Prefetch(
+            'photo_set',
+            queryset=Photo.objects.prefetch_related('tagged_pets', 'like_set')
+        )
+    ).get(slug=pet_slug)
 
-def pet_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    context = {'pet': pet}
+    return render(request, 'pets/pet-details-page.html', context)
+
+def pet_edit(request: HttpRequest, username: str, pet_slug: str) -> HttpResponse:
     return render(request, 'pets/pet-edit-page.html')
 
-def pet_delete(request: HttpRequest, pk: int) -> HttpResponse:
+def pet_delete(request: HttpRequest, username: str, pet_slug: str) -> HttpResponse:
     return render(request, 'pets/pet-delete-page.html')
