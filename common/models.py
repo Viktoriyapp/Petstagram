74cb5f71e22707a674dbfc0244019f4550f12b0d
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-02-10 20:49:18 +0200

    Managed comments

--- common/models.py
+++ common/models.py
@@ -10,6 +10,9 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
     )
 
+    class Meta:
+        ordering = ['-date_and_time_of_publication']
+
 
 class Like(models.Model):
     to_photo = models.ForeignKey(
