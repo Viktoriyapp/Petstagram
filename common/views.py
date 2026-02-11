f7b4ba8d7a4632658709068a3a85fb4b0d84f4c6
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-02-11 17:25:47 +0200

    Added functionallity for comments and search bar

--- common/views.py
+++ common/views.py
@@ -2,20 +2,41 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, resolve_url
 from pyperclip import copy
 
-from common.models import Like
+from common.forms import CommentForm, SearchForm
+from common.models import Like, Comment
 from photos.models import Photo
 
 
 # Create your views here.
 
 def home_page(request: HttpRequest) -> HttpResponse:
+    form = SearchForm(request.GET or None)
     all_photos = (Photo.objects
         .prefetch_related('tagged_pets', 'like_set')
         .all())
 
-    context = {'all_photos': all_photos}
+    if request.GET and form.is_valid(): # if its not an empty dict
+        searched_name = form.cleaned_data['pet_name']
+        all_photos = all_photos.filter(tagged_pets__name__icontains=searched_name)
+
+    context = {
+        'all_photos': all_photos,
+    }
 
     return render(request, 'common/home-page.html', context)
+
+
+def add_comment(request: HttpRequest, photo_pk: int) -> HttpResponse:
+    if request.method == 'POST':
+        photo = Photo.objects.get(pk=photo_pk)
+        form = CommentForm(request.POST) # create the form eith the data / {"text": "Some comment"}
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.to_photo = photo
+            comment.save()
+
+        return redirect(request.META.get('HTTP_REFERER') + f'#{photo_pk}')
 
 
 def like_functionality(request: HttpRequest, photo_pk: int) -> HttpResponse:
